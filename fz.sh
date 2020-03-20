ca3d44fb0d7d3bd44c856de5c386ec2b1a9bd8be
Septem Li <septemhill@gmail.com>
2020-03-20 16:49:03 +0800

    improve performance

--- fz.sh
+++ fz.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 
-# If you don't have `realpath` command on your MacOS, 
-# run `brew install coreutils` first.
+du -hd1 | sed '$d' | sort -h
 
-CPATH=`pwd -P`
-ABSOLUTE=
-
-case $1 in
-	/*) ABSOLUTE=$1;;
-	*) ABSOLUTE=${CPATH}/$1;;
-esac
-
-FZ=
-
-for f in `ls ${ABSOLUTE}`
-do
-	if [ -d "${ABSOLUTE}/${f}" ]; then
-		ABPATH=`realpath ${ABSOLUTE}/$f`
-		FZ="${FZ}"$'\n'$(du -cksh ${ABPATH} 2>/dev/null | head -n 1)
-	fi
-done
-
-echo "${FZ}" | sort -h
